3b62067435b11225037b8bcb9d525216212e6bac
Luis Martínez Alonso <luis0pk0rgmail.com>
2021-03-03 09:04:11 +0100

    leer un log especifico

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -23,6 +23,11 @@ public class LogAnalyzer
         // Create the reader to obtain the data.
         reader = new LogfileReader();
     }
+    
+    public LogAnalyzer(String logName) {
+         hourCounts = new int[24];
+         reader = new LogfileReader(logName);
+    }
 
     /**
      * Analyze the hourly access data from the log file.
